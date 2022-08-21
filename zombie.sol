a5d77a6fb5ed076dcd1462cc2d39d0802c4586bb
Zachary Boelter <zboelter@gmail.com>
2022-08-21 15:47:35 -0700

    create event to be triggered in createZombie function

--- zombie.sol
+++ zombie.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.25;
 
-contract ZombieFactory {                        //name of contract
+contract ZombieFactory {                        //establish contract
+
+    event NewZombie(uint zombieId, string name, uint dna);              //event to be triggered in createZombie function
 
     uint dnaDigits = 16;                        //data type unsigned integer
     uint dnaModulus = 10 ** dnaDigits; 
@@ -13,7 +15,8 @@ contract ZombieFactory {                        //name of contract
     Zombie[] public zombies;                    //create array to store the zombies we create later
 
     function createZombie(string memory _name, uint _dna) public {      //public function that puts zombies in array
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;                //array.push - 1 to get the last zombie we just created
+        emit NewZombie(id, _name, _dna)                                 //fire off event taking all params
     }
 
     function _generateRandomDna(string memory _str) private view returns(uint){     //private function that generates a random dna for zombie
