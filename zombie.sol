2a55cd58813ccee48293275466bcfd148bf053eb
Zachary Boelter <zboelter@gmail.com>
2022-08-21 15:23:21 -0700

    comments for app

--- zombie.sol
+++ zombie.sol
@@ -1,27 +1,27 @@
 pragma solidity ^0.4.25;
 
-contract ZombieFactory {
+contract ZombieFactory {                        //name of contract
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+    uint dnaDigits = 16;                        //data type unsigned integer
+    uint dnaModulus = 10 ** dnaDigits; 
 
-    struct Zombie {
+    struct Zombie {                             //data structure type
         string name;
         uint dna;
     }
 
-    Zombie[] public zombies;
+    Zombie[] public zombies;                    //create array to store the zombies we create later
 
-    function createZombie(string memory _name, uint _dna) public {
+    function createZombie(string memory _name, uint _dna) public {      //public function that puts zombies in array
         zombies.push(Zombie(_name, _dna));
     }
 
-    function _generateRandomDna(string memory _str) private view returns(uint){
+    function _generateRandomDna(string memory _str) private view returns(uint){     //private function that generates a random dna for zombie
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string memory _name) public{
+    function createRandomZombie(string memory _name) public{            //public function that gives zombie dna and name
         uint randDna = _generateRandomDna(_name);
         createZombie(_name, randDna);
     }
