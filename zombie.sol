7dbde6dacb4993c3df7bb258c1063cdfb3416fab
Zachary Boelter <zboelter@gmail.com>
2022-08-21 15:19:52 -0700

    public function that takes the randomDna sequence and gives it to a name, creating a zombie

--- zombie.sol
+++ zombie.sol
@@ -16,8 +16,13 @@ contract ZombieFactory {
         zombies.push(Zombie(_name, _dna));
     }
 
-    function generateRandomDna(string memory _str) private view returns(uint){
+    function _generateRandomDna(string memory _str) private view returns(uint){
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public{
+        uint randDna = _generateRandomDna(_name);
+        createZombie(_name, randDna);
     }
 }
